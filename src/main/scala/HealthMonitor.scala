bf8bee04c27a436e92cb2142f464c724326c5c97
mVilayvanh <mickaelvilayvanh@gmail.com>
2026-03-23 16:42:40 +0100

    defining periodic monitoring job

--- src/main/scala/HealthMonitor.scala
+++ src/main/scala/HealthMonitor.scala
@@ -2,18 +2,36 @@ package upmc.akka.leader
 
 import akka.actor._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
+
 case class Alive(id : Integer)
 case class HowManyMusiciansAreAlive()
 case class MusicianCount(n : Integer)
+case class UpdateMusiciansHealth()
 
 class HealthMonitor extends Actor {
-  private var musicians = List(-1, -1, -1, -1)
+  var musicians = List(-1, -1, -1, -1)
+  var musiciansPingCounter = List(0, 0, 0, 0)
+  val MusiciansN = 4
 
-  def receive = {
+  val PERIODIC_PING_INTERVAL = 2000
+
+  context.system.scheduler.schedule(0 milliseconds,PERIODIC_PING_INTERVAL milliseconds, self, UpdateMusiciansHealth)
+
+  def receive: Receive = {
     case Alive(id) =>
       musicians = musicians.updated(id, 1)
+      musiciansPingCounter = musiciansPingCounter.updated(id, 0)
     case HowManyMusiciansAreAlive =>
       sender ! MusicianCount(musicians.count(m => m == 1))
+    case UpdateMusiciansHealth =>
+      for (i <- 0 to MusiciansN) {
+        musiciansPingCounter = musiciansPingCounter.updated(i, musiciansPingCounter(i) + 1)
+        if (musiciansPingCounter(i) >= 4)
+          musicians = musicians.updated(i, -1)
+      }
   }
 
 }
