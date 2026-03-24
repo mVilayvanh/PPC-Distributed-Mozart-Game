33c0f51ad8b6c67beef702d103007df66c64946f
Noebrt <noe.breton@protonmail.com>
2026-03-24 16:45:27 +0100

    fix octave shift

--- src/main/scala/Player.scala
+++ src/main/scala/Player.scala
@@ -1,87 +1,53 @@
-
 package upmc.akka.leader
-
-import math._
 
 import javax.sound.midi._
 import javax.sound.midi.ShortMessage._
-
+import akka.actor.Actor
 import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext
-import ExecutionContext.Implicits.global
-
-import akka.actor.{Props, Actor, ActorRef, ActorSystem}
-
 
 object PlayerActor {
-  case class MidiNote (pitch:Int, vel:Int, dur:Int, at:Int)
-  val synth = MidiSystem.getSynthesizer()
+  case class MidiNote(pitch: Int, vel: Int, dur: Int, at: Int)
+
+  val synth: Synthesizer = MidiSystem.getSynthesizer()
   synth.open()
-  val rcvr = synth.getReceiver()
+  val rcvr: Receiver = synth.getReceiver()
 
-
-/////////////////////////////////////////////////
-def note_on (pitch:Int, vel:Int, chan:Int): Unit = {
+  def note_on(pitch: Int, vel: Int, chan: Int): Unit = {
     val msg = new ShortMessage
     msg.setMessage(NOTE_ON, chan, pitch, vel)
     rcvr.send(msg, -1)
-}
+  }
 
-def note_off (pitch:Int, chan:Int): Unit = {
+  def note_off(pitch: Int, chan: Int): Unit = {
     val msg = new ShortMessage
-    msg.setMessage(NOTE_ON, chan, pitch, 0)
+    msg.setMessage(NOTE_OFF, chan, pitch, 0)
     rcvr.send(msg, -1)
-}
-
-}
-
-//////////////////////////////////////////////////
-
-class PlayerActor () extends Actor{
-  import DataBaseActor._
-  import PlayerActor._
-
-  def receive = {
-    case Measure (l:List[Chord]) => {
-      println("Player : received measure (normal mode)")
-      l.foreach( n => {
-        n.notes.foreach(note => {
-          self ! MidiNote(note.pitch, note.vol, note.dur, n.date)
-        }
-        )
-
-
-      })
-    }
-    case MidiNote(p,v, d, at) => {
-      context.system.scheduler.scheduleOnce ((at) milliseconds) (note_on (p,v,10))
-      context.system.scheduler.scheduleOnce ((at+d) milliseconds) (note_off (p,10))
-    }
   }
 }
 
-//////////////////////////////////////////////////
-// Mode 2 : transposition d'une octave (+12 demi-tons)
-
-class OctavePlayerActor () extends Actor{
+class PlayerActor(pitchTransform: Int => Int = identity) extends Actor {
   import DataBaseActor._
   import PlayerActor._
+  import context.dispatcher
 
-  val OCTAVE_SHIFT = 12
+  private val channel = 10
 
-  def receive = {
-    case Measure (l:List[Chord]) => {
-      println("Player : received measure (octave transposition mode)")
-      l.foreach( n => {
-        n.notes.foreach(note => {
-          val transposedPitch = math.min(note.pitch + OCTAVE_SHIFT, 127)
-          self ! MidiNote(transposedPitch, note.vol, note.dur, n.date)
-        })
-      })
-    }
-    case MidiNote(p,v, d, at) => {
-      context.system.scheduler.scheduleOnce ((at) milliseconds) (note_on (p,v,10))
-      context.system.scheduler.scheduleOnce ((at+d) milliseconds) (note_off (p,10))
-    }
+  def receive: Receive = {
+    case Measure(chords) =>
+      println("Player : received measure")
+      chords.foreach { chord =>
+        chord.notes.foreach { note =>
+          val newPitch = pitchTransform(note.pitch).max(0).min(127)
+          self ! MidiNote(newPitch, note.vol, note.dur, chord.date)
+        }
+      }
+
+    case MidiNote(p, v, d, at) =>
+      context.system.scheduler.scheduleOnce(at.milliseconds) {
+        note_on(p, v, channel)
+      }
+      context.system.scheduler.scheduleOnce((at + d).milliseconds) {
+        note_off(p, channel)
+      }
   }
 }
